a1c251d5c184ef0dca637e8c46d9abf2b54da959
glebbash <glebbash@gmail.com>
2025-10-15 13:54:16 +0000

    chore: cleanup

--- customer-config/transaction-repository/txr.digital-receipt-consumers.v1.d.ts
+++ customer-config/transaction-repository/txr.digital-receipt-consumers.v1.d.ts
@@ -2,7 +2,6 @@
   JSON schema generated with:
   ```bash
   SCHEMA="customer-config/transaction-repository/txr.digital-receipt-consumers.v1"; \
-  mkdir -p "$(dirname "$SCHEMA.json")"; \
   npx ts-json-schema-generator --no-top-ref --path "$SCHEMA.d.ts" \
     --id "https://raw.githubusercontent.com/extenda/hiiretail-json-schema-registry/master/$SCHEMA.json" \
     | npx fx 'delete this.definitions, this.$schema="https://json-schema.org/draft/2020-12/schema", this' \
