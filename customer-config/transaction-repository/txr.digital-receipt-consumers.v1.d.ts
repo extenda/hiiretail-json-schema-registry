ab4b95e511539fc8d456d7a5820c6352aeaac50d
glebbash <glebbash@gmail.com>
2025-10-15 13:51:49 +0000

    fix: update json schema generation

--- customer-config/transaction-repository/txr.digital-receipt-consumers.v1.d.ts
+++ customer-config/transaction-repository/txr.digital-receipt-consumers.v1.d.ts
@@ -1,11 +1,13 @@
 /*
   JSON schema generated with:
   ```bash
+  SCHEMA="customer-config/transaction-repository/txr.digital-receipt-consumers.v1"; \
+  mkdir -p "$(dirname "$SCHEMA.json")"; \
   npx ts-json-schema-generator --no-top-ref \
-    --id 'https://raw.githubusercontent.com/extenda/hiiretail-json-schema-registry/master/customer-config/transaction-repository/txr.digital-receipt-consumers.v1' \
-    --path 'customer-config/transaction-repository/txr.digital-receipt-consumers.v1.d.ts' \
+    --id "https://raw.githubusercontent.com/extenda/hiiretail-json-schema-registry/master/$SCHEMA.json" \
+    --path "$SCHEMA.d.ts" \
     | npx fx 'delete this.definitions, this.$schema="https://json-schema.org/draft/2020-12/schema", this' \
-    > customer-config/transaction-repository/txr.digital-receipt-consumers.v1.json
+    > "$SCHEMA.json"
   ```
  */
 export type DigitalReceiptConsumers = {
