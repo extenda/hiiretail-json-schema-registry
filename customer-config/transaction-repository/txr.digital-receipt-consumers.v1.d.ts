04d906fae5841a4f6c7df20e99c81ebd4e5bb2ef
glebbash <glebbash@gmail.com>
2025-10-15 13:40:07 +0000

    chore: replace `jq` with `npm fx`

--- customer-config/transaction-repository/txr.digital-receipt-consumers.v1.d.ts
+++ customer-config/transaction-repository/txr.digital-receipt-consumers.v1.d.ts
@@ -4,7 +4,7 @@
   npx ts-json-schema-generator --no-top-ref \
     --id 'https://raw.githubusercontent.com/extenda/hiiretail-json-schema-registry/master/customer-config/transaction-repository/txr.digital-receipt-consumers.v1' \
     --path 'customer-config/transaction-repository/txr.digital-receipt-consumers.v1.d.ts' \
-    | jq 'del(.definitions) + {"$schema":"https://json-schema.org/draft/2020-12/schema"}' \
+    | npx fx 'delete this.definitions, this.$schema="https://json-schema.org/draft/2020-12/schema", this' \
     > customer-config/transaction-repository/txr.digital-receipt-consumers.v1.json
   ```
  */
