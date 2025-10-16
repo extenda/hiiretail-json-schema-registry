03d33e77fdf015524a649dd3bb8230cc8703c9a1
glebbash <glebbash@gmail.com>
2025-10-16 09:51:06 +0000

    chore: remove `onDemand` consumer option

--- customer-config/transaction-repository/txr.digital-receipt-consumers.v1.d.ts
+++ customer-config/transaction-repository/txr.digital-receipt-consumers.v1.d.ts
@@ -1,7 +1,6 @@
 // Generate json schema with: `dts-to-json-schema.sh`
 
 export type DigitalReceiptConsumers = {
-  onDemand?: {},
   externalEvents?: {},
   kivra?: {
     /** @default 'production' */
