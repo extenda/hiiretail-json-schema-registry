0f2473f7dbf46a1e57bb2f1dd6c5e49f88e82a15
glebbash <glebbash@gmail.com>
2025-11-11 21:05:43 +0200

    ci(HII-12450): trigger schema sync

--- customer-config/transaction-repository/txr.digital-receipt-targets.v1.d.ts
+++ customer-config/transaction-repository/txr.digital-receipt-targets.v1.d.ts
@@ -1,7 +1,7 @@
 // Generate json schema with: `dts-to-json-schema.sh`
 
 export type DigitalReceiptTargets = {
-  /** Default implementation, processed internally */
+  /** Default implementation (processed internally) */
   default?: {},
 
   /** Sent to kivra.se */
